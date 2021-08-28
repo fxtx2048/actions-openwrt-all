1f2f9a34092e51e7705deaea6534a3c107f2aca9
fxtx2048 <69618714+fxtx2048@users.noreply.github.com>
2021-08-28 11:35:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 #修改默认主题为argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #修改密码为空，自定义名称
 #ZZZ="package/lean/default-settings/files/zzz-default-settings"
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
