093407e93d26b0e6b7a37e22c00a6fc008cbf949
fxtx2048 <69618714+fxtx2048@users.noreply.github.com>
2021-09-01 11:55:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 #拉取组播代理插件
-git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
-git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
+#git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
+#git clone https://github.com/godros/luci-app-godproxy.git package/lean/luci-app-godproxy
 #修改默认主题为argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #修改密码为空，自定义名称
